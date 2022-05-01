cf861673e199f0431fabb28b08156f1795c882ed
Iaan Lamanosau <iaan.lamanosau@protonmail.com>
2022-05-01 15:15:35 +0300

    Render books in fragment

--- src/components/Library/Library.tsx
+++ src/components/Library/Library.tsx
@@ -1,36 +1,39 @@
 import Book from '../Book/Book';
+import BooksList from './BooksList/BooksList';
 
 import { query, collection } from 'firebase/firestore';
 import { useCollection } from 'react-firebase-hooks/firestore';
 import { db } from '../../App';
-import { User } from 'firebase/auth';
 
 export default function Library({
   onBookEdit,
-  user,
+  userId,
 }: {
   onBookEdit: () => void;
-  user: User;
+  userId: string;
 }) {
-  const booksQuery = query(collection(db, `${user.uid}`));
+  const booksQuery = query(collection(db, userId));
   const [booksSnapshot, loading, error] = useCollection(booksQuery);
 
   return (
     <div className="c-library">
-      {booksSnapshot &&
-        booksSnapshot.docs.map((doc) => {
-          const bookData = doc.data();
-          return (
-            <Book
-              name={bookData.name}
-              author={bookData.author}
-              onEdit={onBookEdit}
-              key={doc.id}
-              id={doc.id}
-              user={user}
-            />
-          );
-        })}
+      {booksSnapshot && (
+        <BooksList>
+          {booksSnapshot.docs.map((doc) => {
+            const bookData = doc.data();
+            return (
+              <Book
+                name={bookData.name}
+                author={bookData.author}
+                onEdit={onBookEdit}
+                key={doc.id}
+                id={doc.id}
+                userId={userId}
+              />
+            );
+          })}
+        </BooksList>
+      )}
       {error && (
         <div className="c-library c-library--error">
           Something went wrong when reading data
