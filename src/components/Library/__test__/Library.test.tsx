198beac6b121631b340135dc6bc1773bda1fc349
Iaan Lamanosau <iaan.lamanosau@protonmail.com>
2022-05-01 15:47:01 +0300

    Write integration test for library

---
+++ src/components/Library/__test__/Library.test.tsx
@@ -0,0 +1,47 @@
+import { screen, render } from '@testing-library/react';
+import Library from '../Library';
+
+jest.mock('firebase/firestore', () => ({
+  __esModule: true,
+  collection: jest.fn(),
+  query: jest.fn(),
+  getFirestore: jest.fn(),
+}));
+
+const mockBooksSnapshot = {
+  docs: [
+    {
+      data: () => ({ name: 'book 1', author: 'author 1' }),
+      id: '1',
+    },
+    {
+      data: () => ({ name: 'book 2', author: 'author 2' }),
+      id: '2',
+    },
+    {
+      data: () => ({ name: 'book 3', author: 'author 3' }),
+      id: '3',
+    },
+  ],
+};
+
+jest.mock('react-firebase-hooks/firestore', () => ({
+  __esModule: true,
+  useCollection: jest.fn().mockImplementation(() => {
+    return [mockBooksSnapshot, false, undefined];
+  }),
+}));
+
+describe('Books quantity', () => {
+  it('Shows books from books snapshot', () => {
+    render(<Library onBookEdit={jest.fn()} userId={'1'} />);
+
+    const nodes = screen.getAllByText(/book/i);
+
+    nodes.forEach((node) => {
+      expect(node).toBeInTheDocument();
+    });
+
+    expect(nodes.length).toBe(mockBooksSnapshot.docs.length);
+  });
+});
