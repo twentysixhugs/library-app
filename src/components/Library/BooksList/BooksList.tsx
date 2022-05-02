2d054b959b7f93c8c1ce98b646dd1aabda540658
Iaan Lamanosau <iaan.lamanosau@protonmail.com>
2022-05-02 20:59:12 +0300

    Render 'start by adding some book'

--- src/components/Library/BooksList/BooksList.tsx
+++ src/components/Library/BooksList/BooksList.tsx
@@ -4,7 +4,16 @@ import './BooksList.css';
 export default function BooksList({
   children,
 }: {
-  children?: React.ReactNode;
+  children?: React.ReactNode[];
 }) {
-  return <div className="c-books-list">{children}</div>;
+  console.log(children);
+  return (
+    <div className="c-books-list">
+      {children && children.length === 0 ? (
+        <span>Start by adding some book</span>
+      ) : (
+        children
+      )}
+    </div>
+  );
 }
